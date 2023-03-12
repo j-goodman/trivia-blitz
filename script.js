a5f3a38ad9c216b93b3df1bd78d92d21177916da
Noni Porter <n@nonis-air.mynetworksettings.com>
2023-03-12 14:27:38 -0400

    Trivia Blitz

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// window.onload = () => {
-//     let rightAnswer = document.getElementsByClassName("right-answer")[0]
-//     let wrongAnswer = document.getElementsByClassName("wrong-answer")[0]
-//     rightAnswer.onclick = rightAnswerClick
-//     wrongAnswer.onclick = wrongAnswerClick
-// }
+window.onload = () => {
+     let rightAnswers = document.getElementsByClassName("right-answer")
+     let wrongAnswers = document.getElementsByClassName("wrong-answer")
+        for (const rightAnswer of rightAnswers) {
+            rightAnswer.onclick = rightAnswerClick
+        }
+        for (const wrongAnswer of wrongAnswers) {
+            wrongAnswer.onclick = wrongAnswerClick
+        }
+        console.log(rightAnswers);
+     }
 
-// let rightAnswerClick = (event) => {
-//     event.target.classList.add("green")
-// }
+ let rightAnswerClick = (event) => {
+     event.target.classList.add("green")
+ }
 
-// let wrongAnswerClick = (event) => {
-//     event.target.classList.add("red")
-// }
+let wrongAnswerClick = (event) => {
+       event.target.classList.add("red")
+     }
