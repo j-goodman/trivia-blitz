dc7711c380a54dc2a71d13185f6f511fd6effc73
j-goodman <johngoodman@pursuit.org>
2023-03-12 12:09:58 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.onload = () => {
+//     let rightAnswer = document.getElementsByClassName("right-answer")[0]
+//     let wrongAnswer = document.getElementsByClassName("wrong-answer")[0]
+//     rightAnswer.onclick = rightAnswerClick
+//     wrongAnswer.onclick = wrongAnswerClick
+// }
+
+// let rightAnswerClick = (event) => {
+//     event.target.classList.add("green")
+// }
+
+// let wrongAnswerClick = (event) => {
+//     event.target.classList.add("red")
+// }
